e64b46c89bcce4bab09945c06da3188ce9c80cbf
rihito <rihito1997@gmail.com>
2021-03-22 05:36:25 +0900

    最終調整

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -2,8 +2,9 @@ class PrototypesController < ApplicationController
   before_action :set_prototype, except: [:index, :new, :create]
   before_action :authenticate_user!, except: [:index, :show]
   before_action :contributor_confirmation, only: [:edit, :update, :destroy]
+  
   def index
-    @prototypes = Prototype.all
+    @prototypes = Prototype.includes(:user)
   end
   def new
     @prototype = Prototype.new
