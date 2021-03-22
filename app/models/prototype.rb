b7cd0ff899917ffac1f98687e43201bb23286018
rihito <rihito1997@gmail.com>
2021-03-22 17:45:22 +0900

    指摘点修正

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -5,7 +5,7 @@ class Prototype < ApplicationRecord
 
   validates :title, presence: true
   validates :catch_copy, presence: true
-  validates :user, presence: true
+  validates :concept, presence: true
   validates :image, presence: true
 
 end
